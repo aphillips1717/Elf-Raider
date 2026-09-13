390b6d93580be0c3dc3c806426f09d628f55c2a1
20176553 <20176553@tafe.wa.edu.au>
2026-09-13 16:17:29 +0800

    Created game file

---
+++ Elf_raider.py
@@ -0,0 +1,5 @@
+print("Game Master\n"
+      "Greetings, traveller, and welcome to the lost city of Threp.\n"
+      "Once the jewel of the West, now overun by the Musc Plague. \n"
+      "Great riches await, for those brave, and strong, enough to enter.")
+user_name = input("Now, tell me, what is your name?")
